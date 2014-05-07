31719343f88edf43e1b9d68582a4117f27b1fa3c
Sean Whitton <spw+git@sdf.org>
2014-05-07 04:47:03 +0000

    ibuffer-vc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -415,7 +415,28 @@
 ;;; ibuffer
 
 (use-package ibuffer
-  :bind ("C-x C-b" . ibuffer))
+  :bind ("C-x C-b" . ibuffer)
+  :config (progn
+            (add-hook 'ibuffer-hook
+                      (lambda ()
+                        (ibuffer-vc-set-filter-groups-by-vc-root)
+                        (unless (eq ibuffer-sorting-mode 'alphabetic)
+                          (ibuffer-do-sort-by-alphabetic))))))
+
+(use-package ibuffer-vc
+  :ensure
+  :commands ibuffer-vc-set-filter-groups-by-vc-root
+  :config (setq ibuffer-formats
+                '((mark modified read-only vc-status-mini " "
+                        (name 18 18 :left :elide)
+                        " "
+                        (size 9 -1 :right)
+                        " "
+                        (mode 16 16 :left :elide)
+                        " "
+                        (vc-status 16 16 :left)
+                        " "
+                        filename-and-process))))
 
 ;;; colour those parentheses
 
