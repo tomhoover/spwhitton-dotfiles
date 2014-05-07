908ce95623736e9f3ce5a0b9ce9783914dbeddaf
Sean Whitton <spw+git@sdf.org>
2014-05-07 04:07:09 +0000

    duplication and commenting functions from Emacs Prelude

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -916,6 +916,52 @@ point reaches the beginning or end of the buffer, stop there."
     (when (= orig-point (point))
       (move-beginning-of-line 1))))
 
+(defun prelude-get-positions-of-line-or-region ()
+  "Return positions (beg . end) of the current line
+or region."
+  (let (beg end)
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (cons beg end)))
+
+(defun prelude-duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (pcase-let* ((origin (point))
+               (`(,beg . ,end) (prelude-get-positions-of-line-or-region))
+               (region (buffer-substring-no-properties beg end)))
+    (-dotimes arg
+      (lambda (n)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point))))
+    (goto-char (+ origin (* (length region) arg) arg))))
+
+(defun prelude-duplicate-and-comment-current-line-or-region (arg)
+  "Duplicates and comments the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (pcase-let* ((origin (point))
+               (`(,beg . ,end) (prelude-get-positions-of-line-or-region))
+               (region (buffer-substring-no-properties beg end)))
+    (comment-or-uncomment-region beg end)
+    (setq end (line-end-position))
+    (-dotimes arg
+      (lambda (n)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point))))
+        (goto-char (+ origin (* (length region) arg) arg))))
+
 ;;;; ---- personal settings ----
 
 ;;; key bindings
@@ -948,6 +994,8 @@ point reaches the beginning or end of the buffer, stop there."
 (unbind-key "C-c m")
 (bind-key "C-c s" 'join-setqs emacs-lisp-mode-map)
 (bind-key "M-/" 'hippie-expand)
+(bind-key "C-c d" 'prelude-duplicate-current-line-or-region)
+(bind-key "C-c M-d" 'prelude-duplicate-and-comment-current-line-or-region)
 
 ;; remap C-a to `smarter-move-beginning-of-line'
 (bind-key "C-a" 'smarter-move-beginning-of-line)
