a90e7ab8d50e3c454b4971877099e631a6ee6a55
Sean Whitton <spw+git@sdf.org>
2014-05-07 03:59:19 +0000

    make C-a in Emacs smarter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -893,6 +893,29 @@ for easier reading and writing"
   (mark-sexp)
   (indent-region (region-beginning) (region-end)))
 
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
 ;;;; ---- personal settings ----
 
 ;;; key bindings
@@ -925,6 +948,9 @@ for easier reading and writing"
 (unbind-key "C-c m")
 (bind-key "C-c s" 'join-setqs emacs-lisp-mode-map)
 (bind-key "M-/" 'hippie-expand)
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(bind-key "C-a" 'smarter-move-beginning-of-line)
 
 (define-key isearch-mode-map "\C-h" 'isearch-delete-char)
 
