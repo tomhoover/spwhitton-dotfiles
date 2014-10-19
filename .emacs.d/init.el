85b2215df5487392a95de0e33f0485212bc8f338
Sean Whitton <spw+git@sdf.org>
2014-10-19 12:05:59 +0900

    unbind C-h in deft

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -589,7 +589,7 @@
                 deft-strip-title-regexp "\\(?:\\#\\+TITLE\\: \\|\\#\\+FILETAGS\\: \\|^%+\\|^[#* ]+\\|-\\*-[[:alpha:]]+-\\*-\\|#+$\\)"))
   :config (progn
             (bind-key "C-w" 'deft-filter-decrement-word deft-mode-map)
-            (bind-key "C-h" 'deft-filter-decrement deft-mode-map)
+            ;; (bind-key "C-h" 'deft-filter-decrement deft-mode-map)
 
             (defadvice deft (before persp-deft activate)
               (projectile-persp-switch-project "~/doc"))))
