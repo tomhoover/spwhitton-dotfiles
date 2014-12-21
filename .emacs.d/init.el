tomhoover/spwhitton-dotfiles

commit 9419a6c98fb2e77ea37c08ed5d9c559bbf195440

make helm-mode completing read more useful

.emacs.d/init.el
@@ -974,6 +974,22 @@ visual state bindings conflicting with god-mode"
 
           (helm-mode)
 
+          ;; helm-mode adds an two arguments to the end of the normal
+          ;; completing-read argument list, the first of which is
+          ;; `cands-in-buffer'.  This makes completion faster because
+          ;; if we type 'mast' to match 'master', we can then hit
+          ;; <enter> to choose master.  Without `cands-in-buffer',
+          ;; hitting enter will select 'mast' (which is intended
+          ;; (https://github.com/emacs-helm/helm/issues/376#issuecomment-30872692),
+          ;; so that helm-mode is consistent with standard Emacs
+          ;; completing-read), and to select master requires an
+          ;; additional C-n keypress which is inconvenient
+          (defadvice helm-completing-read-default-1
+            (around spw/helm-completing-read-cands-in-buffer activate)
+            (interactive)
+            (let ((cands-in-buffer t))
+              ad-do-it))
+
           ;; rebind some keys
           (bind-key "C-w" 'backward-delete-word helm-map)
           (bind-key "C-o" 'helm-select-action helm-map)

commit 6d18e94ee1f5daf0fb79ef4d15597e340010dcb2

fix bug introduced by 9419a6c: when requires-match is nil, helm-comp-read doesn't return a user's custom input when cands-in-buffer is t

.emacs.d/init.el
@@ -987,7 +987,11 @@ visual state bindings conflicting with god-mode"
           (defadvice helm-completing-read-default-1
             (around spw/helm-completing-read-cands-in-buffer activate)
             (interactive)
-            (let ((cands-in-buffer t))
+            ;; unfortunately this doesn't work if require-match is
+            ;; nil, so block that case
+            (let ((require-match (if (not require-match)
+                                     'confirm require-match))
+                  (cands-in-buffer t))
               ad-do-it))
 
           ;; rebind some keys