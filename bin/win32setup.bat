423da23089ce162f3727065e5d15ce0dec09e45d
Sean Whitton <spwhitton@spwhitton.name>
2015-02-06 09:14:05 +0900

    win32 setup script for use with comproc.org

---
+++ bin/win32setup.bat
@@ -0,0 +1,16 @@
+@echo off
+REM See comproc.org for when to use this script
+REM ---- BEGIN WORK
+
+CD %HOMEPATH%
+mklink /J .emacs.d src\dotfiles\.emacs.d
+mklink /J bin src\dotfiles\bin
+mklink /J tmp Desktop
+copy /-y src\dotfiles\home-mrconfig .mrconfig
+copy /-y src\dotfiles\.gitconfig .gitconfig
+copy /-y src\dotfiles\.bashrc .bashrc
+
+REM ---- END   WORK
+ECHO.
+ECHO That should be everything set-up.
+pause
