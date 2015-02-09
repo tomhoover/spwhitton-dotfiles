12ec8bf3eade2bd8c10b2a04b4e089b08ec52dbb
Sean Whitton <spwhitton@spwhitton.name>
2015-02-10 08:37:16 +0900

    win32setup.bat updated to be non-interactive (though therefore slightly dangerous) and called by mr fixups

--- bin/win32setup.bat
+++ bin/win32setup.bat
@@ -6,12 +6,12 @@ CD %HOMEPATH%
 mklink /J .emacs.d src\dotfiles\.emacs.d
 mklink /J bin src\dotfiles\bin
 mklink /J tmp Desktop
-copy /-y src\dotfiles\home-mrconfig .mrconfig
-copy /-y src\dotfiles\.gitconfig .gitconfig
-copy /-y src\dotfiles\.bashrc .bashrc
-copy /-y src\dotfiles\.shenv .shenv
+copy /y src\dotfiles\home-mrconfig .mrconfig
+copy /y src\dotfiles\.gitconfig .gitconfig
+copy /y src\dotfiles\.bashrc .bashrc
+copy /y src\dotfiles\.shenv .shenv
 
 REM ---- END   WORK
 ECHO.
 ECHO That should be everything set-up.
-pause
+REM pause
