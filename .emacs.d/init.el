31d477197cc8bc4329dffc045cf109cac120c691
Sean Whitton <spwhitton@spwhitton.name>
2015-04-01 15:43:05 +0900

    good key chords reference

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1038,6 +1038,30 @@
             (add-hook 'god-mode-enabled-hook 'spw/god-mode-update-modeline)
             (add-hook 'god-mode-disabled-hook 'spw/god-mode-update-modeline)))
 
+;; good key chords from
+;; <http://www.johndcook.com/blog/2015/02/01/rare-bigrams/> via
+;; <https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-key-chord.el>
+
+;; BF BG BQ BX BZ
+;; CJ CV
+;; DX DZ
+;; FB FJ FQ FV FZ
+;; GB GJ GP GQ GV GX GZ
+;; HJ HQ HV
+;; JJ JC JF JG JH JK JL JM JP JQ JS JT JV JW JX JY JZ
+;; KK KJ KQ KV KX KZ
+;; LJ LQ
+;; MJ MQ MV MX MZ
+;; PG PJ PQ PV PZ
+;; QQ QB QF QG QH QJ QK QL QM QP QT QV QW QX QY QZ
+;; SJ SX SZ
+;; TJ TQ
+;; VV VC VF VG VH VJ VK VM VP VQ VW VZ
+;; WW WJ WQ WV WX WZ
+;; XB XD XG XJ XK XM XQ XS XW XZ
+;; YY YJ YQ
+;; ZB ZD ZF ZG ZJ ZK ZM ZP ZQ ZS ZV ZW ZX
+
 
 
 ;;;; ---- functions ----
