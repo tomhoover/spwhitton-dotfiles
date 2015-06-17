5bdb1226b0190efef160c05e17bc34184d60a599
Sean Whitton <spwhitton@spwhitton.name>
2015-06-17 19:16:53 +0900

    xmonad conf tidying

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,14 +4,17 @@ import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Actions.CycleWS (toggleWS)
 import XMonad.Actions.SpawnOn (manageSpawn, spawnOn)
 import qualified XMonad.StackSet as W
+import XMonad.Hooks.ManageDocks
 
 myMod = mod4Mask
 
 main = xmonad $ xfceConfig
-    { terminal   = "urxvt"
-    , modMask    = myMod
-    , workspaces = myWorkspaces
-    , manageHook = manageSpawn <+> manageHook xfceConfig
+
+    { terminal       = "urxvt"
+    , modMask        = myMod
+    , workspaces     = myWorkspaces
+    , manageHook     = manageSpawn <+> manageHook xfceConfig
+    -- , startupHook    = myStartup
     -- , borderWidth = 3
     } `additionalKeys` myKeys
 
@@ -19,6 +22,17 @@ myEditor = "emacsclient -c -n -e '(switch-to-buffer nil)'"
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "web"]
 
 myKeys = [ ((myMod, xK_e), spawn myEditor)
+
+           -- launchers
+         , ((myMod, xK_f), spawnOn "web" "iceweasel")
+         , ((myMod .|. shiftMask, xK_m), spawn "urxvtcd -e mutt")
+
+           -- workspaces
          , ((myMod, xK_grave), toggleWS)
          , ((myMod, xK_0), windows $ W.greedyView "web")
-         , ((myMod, xK_f), spawnOn "web" "iceweasel")]
+         ]
+
+-- myStartup = do
+
+--     -- hide the Xfce panel to begin with
+--     sendMessage ToggleStruts
