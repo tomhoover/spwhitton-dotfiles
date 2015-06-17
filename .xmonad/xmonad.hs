16f0194acfae261a6fc248889d485572e291fe0d
Sean Whitton <spwhitton@spwhitton.name>
2015-06-17 20:42:24 +0900

    attempt to handle completion window & prefix key

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,13 @@ import XMonad.Actions.CycleWS (toggleWS)
 import XMonad.Actions.SpawnOn (manageSpawn, spawnOn)
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Actions.Submap
+
+import Data.List (isInfixOf)
+import Control.Arrow
+import Data.Bits
+import qualified Data.Map as M
 
 myMod = mod4Mask
 
@@ -13,9 +20,10 @@ main = xmonad $ xfceConfig
     { terminal       = "urxvt"
     , modMask        = myMod
     , workspaces     = myWorkspaces
-    , manageHook     = manageSpawn <+> manageHook xfceConfig
+    , manageHook     = manageSpawn XMonad.<+> myManageHook XMonad.<+> manageHook xfceConfig
     -- , startupHook    = myStartup
     -- , borderWidth = 3
+    , keys = addPrefix (controlMask, xK_m) (keys xfceConfig)
     } `additionalKeys` myKeys
 
 myEditor = "emacsclient -c -n -e '(switch-to-buffer nil)'"
@@ -29,7 +37,7 @@ myKeys = [ ((myMod, xK_e), spawn myEditor)
          , ((myMod .|. shiftMask, xK_m), spawn "urxvtcd -e mutt")
 
            -- workspaces
-         , ((myMod, xK_grave), toggleWS)
+         , ((myMod, xK_semicolon), toggleWS)
          , ((myMod, xK_0), windows $ W.greedyView "web")
          ]
 
@@ -37,3 +45,20 @@ myKeys = [ ((myMod, xK_e), spawn myEditor)
 
 --     -- hide the Xfce panel to begin with
 --     sendMessage ToggleStruts
+
+myManageHook = composeOne
+               [ checkDock -?> doIgnore
+               , isDialog               -?> doFloat
+               , className =? "Gimp"    -?> doFloat
+               , className =? "MPlayer" -?> doFloat
+               , title =? "*Completions*" -?> doF W.focusDown
+               , title =? "*Ido Completions*" -?> doF W.focusDown
+               , return True -?> doF W.swapDown
+               ]
+
+-- from <http://kojevnikov.com/xmonad-metacity-gnome.html>
+addPrefix p ms conf =
+    M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
+  where
+    mod     = modMask conf
+    chopMod = (.&. complement mod)
