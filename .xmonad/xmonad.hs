05090177238fb3c27ab799f23390068319f4e438
Sean Whitton <spwhitton@spwhitton.name>
2015-06-19 21:54:24 +0900

    tidy up xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,47 +14,52 @@ import Data.Bits
 import qualified Data.Map as M
 import Control.Monad (liftM2)
 
-myMod = mod4Mask
-
 main = xmonad $ addMyKeys $ xfceConfig
 
-    { terminal           = "urxvtcd"
+    { terminal           = myTerm
     , normalBorderColor  = "#3F3F3F"
     , focusedBorderColor = "#656555"
+    , borderWidth        = 2
     , modMask            = myMod
     , workspaces         = myWorkspaces
-                           -- I would prefer to have newly created
-                           -- windows always inserted at the top of
-                           -- the non-master area.  But insertPosition
-                           -- doesn't support that so let's always put
-                           -- them at the end.
+
+-- I would prefer to have newly created windows always inserted at the
+-- top of the non-master area.  But insertPosition doesn't support
+-- that so let's always put them at the end.
     , manageHook         = insertPosition End Newer
                            <+> myManageHook
                            <+> manageHook xfceConfig
-    -- , startupHook     = myStartup
-    , borderWidth        = 2
     }
 
-myEditor = "emacsclient -c -n -e '(switch-to-buffer nil)'"
+-- basic preferences
+
+myMod        = mod4Mask
+myTerm       = "urxvtcd"
+myEditor     = "emacsclient -c -n -e '(switch-to-buffer nil)'"
+myBrowser    = "iceweasel"
 myWorkspaces = ["one", "two", "web", "misc"]
 
-myPrefixedKeys = [ ("e", spawn myEditor)
+-- key bindings
+
+myPrefixedKeys = [ ("i", spawn "xmousetidy")
 
                    -- launchers
-                 , ("g w", spawn "iceweasel")
-                 , ("s", spawn "urxvtcd")
+                 , ("g w", spawn myBrowser)
+                 , ("g e", spawn myEditor)
+                 , ("g g", spawn myTerm)
+                 , ("g m", spawn $ inMyTerm "mutt")
+
+                   -- window management
                  , ("o", windows W.focusDown)
                  , ("S-o", windows W.focusUp)
-                 , ("i", spawn "xmousetidy")
-                 , ("S-m", spawn "urxvtcd -e mutt")
 
                    -- workspaces
                  , ("C-i", toggleWS)
                  ]
 
-myKeys = [ ("M4-j", windows W.focusDown)
-         , ("M4-k", windows W.focusUp)
-         ]
+myUnprefixedKeys = [ ("M4-j", windows W.focusDown)
+                   , ("M4-k", windows W.focusUp)
+                   ]
 
 myManageHook = composeOne
                [ checkDock -?> doIgnore
@@ -67,12 +72,19 @@ myManageHook = composeOne
 
 myUnwantedKeys = ["M-e"]
 
+-- helper functions
+
+inMyTerm     :: String -> String
+inMyTerm cmd = unwords $ [myTerm, "-e", cmd]
+
 addMyKeys                   = addMyUnprefixed . prefixMainMap . addMyPrefixed . removeSomeDefaults
   where
     removeSomeDefaults conf = conf `removeKeysP` myUnwantedKeys
     addMyPrefixed conf      = conf `additionalKeysP` myPrefixedKeys
-    prefixMainMap conf      = conf { keys = addPrefix (controlMask, xK_i) (keys conf) }
-    addMyUnprefixed conf    = conf `additionalKeysP` myKeys
+    prefixMainMap conf      = conf { keys = addPrefix
+                                            (controlMask, xK_i)
+                                            (keys conf) }
+    addMyUnprefixed conf    = conf `additionalKeysP` myUnprefixedKeys
 
 -- from <http://kojevnikov.com/xmonad-metacity-gnome.html>
 addPrefix p ms conf =
