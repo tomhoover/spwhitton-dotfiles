1493152627752ebeb203db6569dc75ce610ccb5b
Sean Whitton <spwhitton@spwhitton.name>
2015-10-14 20:38:52 -0700

    dasl-setup.bat

---
+++ bin/dasl-setup.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir C:\%HOMEPATH%\SPW
+copy /y lib\putty.exe.reg C:\%HOMEPATH%\SPW
+copy /y lib\putty.exe-empty.reg C:\%HOMEPATH%\SPW
+copy /y lib\spwhitton@putty.ppk C:\%HOMEPATH%\SPW
+copy /y bin\ctrlswapcaps.exe C:\%HOMEPATH%\SPW
+explorer "C:\%HOMEPATH%\SPW"
