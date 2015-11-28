9438b76df4ac1191b5d127c806350830bacd2c27
Sean Whitton <spwhitton@spwhitton.name>
2015-11-28 13:30:37 -0700

    simplify Haskell init right down

--- .emacs.d/init-haskell.el
+++ .emacs.d/init-haskell.el
@@ -2,21 +2,11 @@
 
 ;;; Commentary:
 
-;; There are a lot of choices to make in setting up Emacs for writing
-;; Haskell.  In this file, choices made that I think are a good
-;; balance of current best practices and being conservative:
-
-;; - flycheck over flymake
-;; - ghc-mod over ghci-ng
-;; - hi2 for indentation
-;; - stylish-haskell over hindent
-;; - company over AC
-
 ;;; Installation:
 
 ;; Needs (use-package flycheck) in init.el.
 
-;; Needs exes from Hackage: structured-haskell-mode, ghc-mod, stylish-haskell
+;; Needs exes from Hackage: stylish-haskell
 
 ;;; Code:
 
@@ -37,6 +27,13 @@
 
   :config
 
+  (setq haskell-indentation-layout-offset 4
+        haskell-indentation-left-offset 4
+        haskell-indentation-show-indentations nil
+        haskell-tags-on-save t
+        haskell-process-suggest-remove-import-lines t
+        haskell-mode-contextual-import-completion nil)
+
   ;; Turn on an appropriate indentation mode.  Use
   ;; `haskell-indentation-mode' by default, but if our .dir-locals.el
   ;; specifies `indent-tabs-mode' we should use my
@@ -50,13 +47,15 @@
                             (haskell-indentation-mode)))
                         nil t))) ; local hook
 
-  ;; Start up all my usual minor modes and bindings.
-  (add-hook 'haskell-mode-hook 'spw/haskell-mode-hook)
-  (add-hook 'after-save-hook 'spw/haskell-cabal-mode-save-hook)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
+  (add-hook 'haskell-mode-hook 'capitalized-words-mode)
+  ;; (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  ;; (diminish 'interactive-haskell-mode)
 
-  (setq haskell-tags-on-save t
-        haskell-process-suggest-remove-import-lines t
-        haskell-mode-contextual-import-completion nil))
+  ;; SmartParens interacts badly with haskell-indentation-mode so turn
+  ;; it off for now
+  (add-hook 'haskell-mode-hook 'turn-off-smartparens-mode)
+  (add-hook 'haskell-mode-hook 'electric-pair-mode))
 
 ;;; Try to kill off flymake since init.el is starting flycheck.  Also
 ;;; remove a call to flymake-mode (add the call to `ghc-init' back in
@@ -75,75 +74,6 @@
     (remove-hook 'haskell-mode-hook (lambda ()
                                       (ghc-init)
                                       (flymake-mode)))))
-
-;;; startup hook
-
-(defun spw/haskell-mode-hook ()
-  "Haskell mode startup stuff."
-  (interactive)
-
-  ;; When I've created a stack.yaml in the project root, use stack to
-  ;; run ghci.
-  ;; (when (f-exists? (projectile-expand-root "stack.yaml"))
-  ;;   (setq-local haskell-process-type 'ghci)
-  ;;   (setq-local haskell-process-path-ghci "stack")
-  ;;   (setq-local haskell-process-args-ghci '("ghci"))
-  ;;   (setq-local flycheck-haskell-ghc-executable "stack")
-  ;;   (setq-local flycheck-ghc-args '("ghc")))
-
-  ;; When I've created a shell.nix in the project root, use nix-shell
-  ;; to run ghci and cabal.
-  ;; (when (f-exists? (projectile-expand-root "shell.nix"))
-  ;;   (setq-local haskell-process-type 'cabal-repl)
-  ;;   (setq-local
-  ;;    haskell-process-wrapper-function
-  ;;    (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
-  ;;                           (list (mapconcat 'identity argv " "))))))
-
-  ;;; basic minor modes
-
-  (turn-on-haskell-doc)
-  (capitalized-words-mode)
-  (diminish 'capitalized-words-mode)
-  (interactive-haskell-mode)
-  (diminish 'interactive-haskell-mode)
-  (flymake-mode 0)
-  (smartparens-mode 0)
-
-  ;;; make sure haskell-flycheck checker being used?
-
-  ;; (when (fboundp 'flycheck-disable-checker)
-  ;;   (flycheck-disable-checker 'haskell-ghc))
-
-  ;; until we get ghc-mod working, use dabbrev
-  (company-mode 0)
-  (bind-key "M-/" 'dabbrev-expand interactive-haskell-mode-map)
-
-  (bind-key "C-c C-b" 'haskell-interactive-bring interactive-haskell-mode-map)
-  ;; (bind-key "C-x C-s" 'haskell-mode-save-buffer interactive-haskell-mode-map)
-
-  ;; suggested bindings from Chris Done
-
-  ;; should be moved into use-package declaration above (requires some
-  ;; care)
-
-  ;; (define-key interactive-haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
-  ;; (define-key interactive-haskell-mode-map (kbd "C-?") 'haskell-mode-find-uses)
-  ;; (define-key interactive-haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
-
-  (bind-key "C-c C-t" 'haskell-process-do-type interactive-haskell-mode-map)
-  (bind-key "C-c C-i" 'haskell-process-do-info interactive-haskell-mode-map)
-  (bind-key "C-c C-r" 'haskell-process-restart interactive-haskell-mode-map)
-  (bind-key "SPC"     'haskell-mode-contextual-space haskell-mode-map))
-
-;; save hook
-
-(defun spw/haskell-cabal-mode-save-hook ()
-  "Regenerate shell.nix if it exists."
-  (interactive)
-  (when (and (eq major-mode 'haskell-cabal-mode)
-             (f-exists? (f-join default-directory "shell.nix")))
-    (call-process "/bin/sh" nil t nil "-c" "cabal2nix --shell . > shell.nix")))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
