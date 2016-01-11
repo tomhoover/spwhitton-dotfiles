acc60e049e7d3cd4ff042c914d03198c9e5104f1
Sean Whitton <spwhitton@spwhitton.name>
2016-01-11 11:46:45 -0700

    improve subtrees.sh

--- .emacs.d/pkg/subtrees.sh
+++ .emacs.d/pkg/subtrees.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$1" = "--clear-elc" ]; then
+    find -L $HOME/.emacs.d -name "*.elc" -delete
+    exit 0
+fi
+
 # make haskell-mode autoloads
 (
     # must cd to inside ~/src/dotfiles.  If change to ~/.emacs.d/blah,
@@ -16,8 +21,6 @@
 
 # byte-compile anything that needs to be
 
-# TODO this doesn't follow symbolic link into pkg!
-
 emacs --batch \
-      --eval '(byte-recompile-directory user-emacs-directory 0)' \
+      --eval '(byte-recompile-directory "$HOME/src/dotfiles/.emacs.d" 0)' \
       2>&1 | tail -n1           # output only the summary line
