927b3a118a40787b74c8cdf0d351681726495de6
Sean Whitton <spwhitton@spwhitton.name>
2016-01-26 13:10:25 -0700

    restore M4-S-i binding

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -76,6 +76,7 @@ myKeys = [ ("M4-h", spawn "xmousetidy")
            -- window & workspace management
          , ("M4-'", withFocused (sendMessage . maximizeRestore))
          , ("M4-;", toggleWS)
+         , ("M4-S-i", kill)
 
            -- When locking the screen, also clear out my SSH key.
            -- Otherwise it lasts until I log off.  See GNOME bugzilla
