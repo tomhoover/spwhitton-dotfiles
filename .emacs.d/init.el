b12a7a4a8f5d6281393b2fc15dd87040e74eb792
Sean Whitton <spwhitton@spwhitton.name>
2017-01-28 15:31:06 -0700

    move comments about emacs server to ~/bin/emacscd

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -168,17 +168,6 @@
 (unless (eq system-type 'windows-nt)
   (set-time-zone-rule "/usr/share/zoneinfo/America/Phoenix"))
 
-;;; be sure to start the server
-
-;; According to <http://emacs.stackexchange.com/a/12789> this might be
-;; causing Emacs to fail to start up properly sometimes after a
-;; reboot, giving me "Error reading from stdin".  I don't see how that
-;; would be but since I'm starting with --daemon anyway, let's try
-;; commenting it out.
-
-;; (require 'server)
-;; (unless (server-running-p)
-;;   (server-start))
 
 
 ;;; save my places in buffers; this is all the session management I need
