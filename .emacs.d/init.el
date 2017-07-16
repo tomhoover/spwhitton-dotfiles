3f5e58f4fde91ec51a63f318f5b306b6c94a534b
Sean Whitton <spwhitton@spwhitton.name>
2017-07-16 14:52:59 -0700

    convert ido TRAMP fix to advice

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -499,7 +499,7 @@ https://github.com/company-mode/company-mode/issues/94#issuecomment-40884387"
 
   ;; clean out remote paths from ~/.emacs.d/ido.last
   ;; from JoeBloggs on the Emacs wiki
-  (defun ido-remove-tramp-from-cache nil
+  (defun spw--ido-remove-tramp-from-cache nil
     "Remove any TRAMP entries from `ido-dir-file-cache'.
     This stops tramp from trying to connect to remote hosts on emacs startup,
     which can be very annoying."
@@ -509,10 +509,7 @@ https://github.com/company-mode/company-mode/issues/94#issuecomment-40884387"
 	   (lambda (x)
 	     (string-match "/\\(rsh\\|ssh\\|telnet\\|su\\|sudo\\|sshx\\|krlogin\\|ksu\\|rcp\\|scp\\|rsync\\|scpx\\|fcp\\|nc\\|ftp\\|smb\\|adb\\):" (car x)))
 	   ido-dir-file-cache)))
-  ;; redefine `ido-kill-emacs-hook' so that cache is cleaned before being saved
-  (defun ido-kill-emacs-hook ()
-    (ido-remove-tramp-from-cache)
-    (ido-save-history)))
+  (advice-add 'ido-kill-emacs-hook :before #'spw--ido-remove-tramp-from-cache))
 
 ;;; ebib for editing BiBTeX databases
 
