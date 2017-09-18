9de66760c393fb30553cf91c2e45ca018f47c1d8
Sean Whitton <spwhitton@spwhitton.name>
2017-09-18 10:45:59 -0700

    first version of spwd20-initiative (doesn't work yet)

--- .emacs.d/site-lisp/spwd20.el
+++ .emacs.d/site-lisp/spwd20.el
@@ -33,6 +33,9 @@
 (require 'seq)
 (require 'dash)
 
+(defcustom spwd20-party nil
+  "Party initiative modifiers.")
+
 (defun spwd20--roll (exp)
   "Evaluate dice roll expression EXP.
 
@@ -63,7 +66,41 @@ the best N of them, e.g., 4d6k3."
             rolls))))))
 
 (defun spwd20-initiative ()
-  "Generates an Org-mode table with initiative order and monster HP.")
+  "Generates an Org-mode table with initiative order and monster HP."
+  (interactive "*")
+  (let ((rows))
+    (let (name-input init-input hd-input num-input)
+      (loop
+       do (setq name-input (read-string "Monster/NPC name (blank when done): "))
+       (when (> (length name-input) 0)
+         (setq init-input (read-string (concat name-input "'s init modifier: "))
+               hd-input (read-string (concat name-input "'s hit points: "))
+               num-input (string-to-int
+                          (read-string (concat "How many " name-input "? "))))
+         (while (> num-input 0)
+           (let ((init (int-to-string
+                        (spwd20--roll (concat
+                                       "1d20"
+                                       (spwd20--num-to-term init-input)))))
+                 (hp (int-to-string (spwd20--roll hd-input))))
+             (push (list "" name-input init-input init hp "0") rows))
+           (setq num-input (- num-input 1))))
+       while (-all? (lambda (x) (> (length x) 0))
+                    (list name-input init-input hd-input))))
+    (dolist (pc spwd20-party)
+      (let ((init (read-string (concat (car pc) "'s initiative roll: "))))
+        (push (list "" (car pc) (int-to-string (car (cdr pc))) init "-" "-")
+              rows)))
+    (insert "Round of combat: 1\n\n|Creature|Mod|Init|HP|Damage|Status|\n|-\n")
+    (dolist (row rows)
+      (dolist (cell row)
+        (insert "|" cell))
+      (insert "|\n"))))
+
+(defun spwd20--num-to-term (n)
+  (if (>= n 0)
+      (concat "+" (int-to-string n))
+    (int-to-string n)))
 
 ;;;###autoload
 (define-minor-mode spwd20-mode
