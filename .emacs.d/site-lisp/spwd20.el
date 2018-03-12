51ca024af23dd544b0b2b1debd7ab2d323c5e9ab
Sean Whitton <spwhitton@spwhitton.name>
2018-03-12 16:44:29 -0700

    TODO

--- .emacs.d/site-lisp/spwd20.el
+++ .emacs.d/site-lisp/spwd20.el
@@ -44,6 +44,11 @@
 
 ;;; TODO dice expression roller shows results for each die
 
+;;; TODO rewrite combat to use a data strcuture holding all data from
+;;; the table with functions to read, write and update an Org table.
+;;; Then e.g. taking damage is done by reading table, updating struct
+;;; and writing it back to the buffer
+
 ;;; Code:
 
 (require 's)
