2875d62f0de6eedc5db04ca467d2859c8dcd781d
Sean Whitton <spwhitton@spwhitton.name>
2018-05-11 09:41:23 -0700

    embed copies of stow and mr in dotfiles repo more sensibly

--- bin/bstraph.sh
+++ bin/bstraph.sh
@@ -8,63 +8,6 @@
 
 set -e
 cd "$HOME"
-mkdir -p "$HOME/local/src" "$HOME/local/bin"
-
-# use http and checksum the tarball, rather than https:// which often
-# fails on Debian hosts due to old ca-certificates
-STOWURL="http://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz"
-STOWSHA256SUM="8b89d79939cf9ae87d2f223bb36a3b2d0c66775b62aeb9953c6d33dab40d3c2b  stow-2.2.0.tar.gz"
-
-# ---- Ensure we have both mr(1) and stow(1) in our PATH
-
-if ! which stow >/dev/null; then
-    (
-        cd $HOME/local/src
-        rm -rf stow-*
-        curl -O $STOWURL
-        stowsha256sum=$(sha256sum stow-*.tar.gz)
-        if ! [ "$STOWSHA256SUM" = "$stowsha256sum" ]; then
-            echo >&2 "bstraph.sh: ERROR: stow tarball checksum mismatch"
-            rm -rf stow-*
-            exit 1
-        fi
-
-        tar xfz stow-*.tar.gz
-        cd $(basename $STOWURL | sed -e 's/.tar.gz//')
-
-        # we need to use gmake on BSD
-        if which gmake; then
-            ./configure --prefix=$HOME/local && gmake install
-        else
-            ./configure --prefix=$HOME/local && make install
-        fi
-    )
-fi
-
-# TODO check version of mr available on system, rather than
-# unconditionally using the one in src/dotfiles/bin/
-#if ! which mr >/dev/null; then
-    mkdir -p $HOME/local/bin
-    cp $HOME/src/dotfiles/bin/mr $HOME/local/bin
-#fi
-
-# ---- Verify mr(1) and stow(1) in our PATH
-
-# .shenv adds ~/local/bin to PATH if that dir exists, so we have to
-# call it again now we've created it
-. $HOME/src/dotfiles/.shenv
-hash -r
-
-# TODO also check version of stow is new enough
-
-if ! which stow >/dev/null; then
-    echo "still can't find stow :(" >&2
-    exit 1
-fi
-if ! which mr >/dev/null; then
-    echo "still can't find mr :(" >&2
-    exit 1
-fi
 
 # ---- Perform the bootstrap
 
