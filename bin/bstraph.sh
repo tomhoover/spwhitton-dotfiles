b1243d14ee7f5497bbb0d201d6285f7f0a57ff4e
Sean Whitton <spwhitton@spwhitton.name>
2018-05-15 20:41:43 -0700

    try to make bstraph.sh a bit more robust

--- bin/bstraph.sh
+++ bin/bstraph.sh
@@ -7,19 +7,19 @@
 # definitely be POSIX sh
 
 set -e
-cd "$HOME"
 
 # ---- Perform the bootstrap
 
-mr -t --config src/dotfiles/home-mrconfig -d $HOME/src/dotfiles fixups
-mr -d src/dotfiles stow
+mr -t --config $HOME/src/dotfiles/home-mrconfig -d $HOME/src/dotfiles fixups
+mr -d $HOME/src/dotfiles stow
 
 echo "I: dotfiles bootstrap successful"
 
 # ---- Check if there is a host-specific branch
 
-if ! [ "$(hostname)" = "master" ] \
-        && ( cd "$HOME/src/dotfiles" && git branch -a | grep origin/$(hostname) ); then
+hostname="$(hostname -s)"
+if ! [ "$hostname" = "master" ] \
+        && ( cd "$HOME/src/dotfiles" && git branch -a | grep "origin/$hostname" ); then
     echo "I: there is a host-specific dotfiles branch for this host"
     echo "I: consider checking it out"
 fi
