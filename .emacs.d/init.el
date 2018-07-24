4e5ba7f1d38899c6bcb14ef1ee39a7af4879bcb7
Sean Whitton <spwhitton@spwhitton.name>
2018-07-24 17:13:25 +0800

    some tweaks to notmuch-show

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1039,6 +1039,14 @@ Passes ARG to `projectile-switch-project-by-name'."
   ;; newer e-mails at the top
   (setq notmuch-search-oldest-first nil)
 
+  ;; pretty inline diffs
+  (add-hook 'notmuch-show-insert-text/plain-hook 'notmuch-wash-convert-inline-patch-to-part)
+
+  ;; don't collapse citations please
+  ;; TODO this has the unpleasant side effect that cited text is no
+  ;; longer fontlocked
+  ;;(remove-hook 'notmuch-show-insert-text/plain-hook 'notmuch-wash-excerpt-citations)
+
   ;; this ensures that hitting C-x m right after Emacs starts yields a
   ;; message with the correct From: address and User-Agent header, etc.
   (defun compose-mail--load-notmuch (&rest ignore)
